fe903cdbc2d91556529b134c68bcbf01676e5e4c
Akkewach Yodsomboon <akkewach@planforfit.com>
2021-03-01 15:26:56 +0700

    ปรับแต่ง frontend import members

--- src/views/importMembers.js
+++ src/views/importMembers.js
@@ -11,7 +11,8 @@ class ImportMembers extends Component {
     this.state = {
       emails: [],
       selectedStartDate: null,
-      selectedExpireDate: null
+      selectedExpireDate: null,
+      selectedFile: null
     };
     this.fileSelectedHandler = this.fileSelectedHandler.bind(this);
   }
@@ -30,19 +31,29 @@ class ImportMembers extends Component {
   fileSelectedHandler = event => {
     var { emails } = this.state;
     const Papa = require('papaparse');
-    Papa.parse(document.getElementById('upload-csv').files[0], {
-      download: true,
-      header: false,
-      complete: function (results) {
-        var data = results.data;
-        console.log("data : ", data);
-        for (var i = 1; i <= data.length - 1; i++) { // i = 1 เพราะว่า 0 เป็นหัวcolumn
-          emails.push(data[i][0]);
-          //console.log(`emails ${i} : `, emails);
+
+    this.setState({
+      selectedFile: null
+    })
+
+    if (document.getElementById('upload-csv').files[0]) {
+      Papa.parse(document.getElementById('upload-csv').files[0], {
+        download: true,
+        header: false,
+        complete: function (results) {
+          var data = results.data;
+          console.log("data : ", data);
+          for (var i = 1; i <= data.length - 1; i++) { // i = 1 เพราะว่า 0 เป็นหัวcolumn
+            emails.push(data[i][0]);
+            //console.log(`emails ${i} : `, emails);
+          }
+          //console.log("emails ALL :", emails);
         }
-        //console.log("emails ALL :", emails);
-      }
-    });
+      });
+      this.setState({
+        selectedFile: document.getElementById('upload-csv').files[0]
+      })
+    }
   }
 
   onUpload() {
@@ -69,34 +80,101 @@ class ImportMembers extends Component {
   render() {
     const { selectedStartDate, selectedExpireDate } = this.state;
     return (
-      <div>
+      <div className="row">
         <h1>.</h1>
-        <h1>.</h1>
-        <h1>.</h1>
-        <h1>CSV Emails</h1>
-        <input type="file" id="upload-csv" accept=".csv" onChange={this.fileSelectedHandler} />
-        <h1>start_date</h1>
-        <DatePicker
-          dateFormat='yyyy/MM/dd'
-          selected={selectedStartDate}
-          onChange={date => this.setState({ selectedStartDate: date })}
-          isClearable
-          showYearDropdown
-          showMonthDropdown
-          scrollableMonthYearDropdown
-        />
-        <h1>expire_date</h1>
-        <DatePicker
-          dateFormat='yyyy/MM/dd'
-          selected={selectedExpireDate}
-          onChange={date => this.setState({ selectedExpireDate: date })}
-          isClearable
-          showYearDropdown
-          showMonthDropdown
-          scrollableMonthYearDropdown
-        />
-        <h1>.</h1>
-        <button onClick={() => this.onUpload()}>UPLOAD</button>
+        <div className="card mt-5 mb-3 col-lg-12">
+          <div className="card-body">
+
+            <h1 className="mb-5">เพิ่มสมาชิก</h1>
+            <h5>อัปโหลดไฟล์อีเมล
+              <span className="h6 ml-3" style={{ color: "red" }}>
+                *ไฟล์นามสกุล .csv
+              </span>
+            </h5>
+            <input
+              type="file"
+              id="upload-csv"
+              accept=".csv"
+              ref={fileInput => this.fileInput = fileInput}
+              onChange={this.fileSelectedHandler}
+              style={{ display: "none" }}
+            />
+            <div className="mb-5 col-lg-9 col-md-12">
+              <div className="input-group mb-2">
+                <div className="input-group-prepend">
+                  <button
+                    class=""
+                    onClick={() => this.fileInput.click()}
+                    style={{ background: "#333333", color: "white" }}
+                  >
+                    อัปโหลด
+              </button>
+                </div>
+                <input
+                  type="text"
+                  class="form-control "
+                  value={(this.state.selectedFile) ? this.state.selectedFile.name : ""}
+                />
+              </div>
+              {
+                ((this.state.statusMaualPayment === "fail") && !(this.state.selectedFile)) &&
+                <small id="emailHelp" className="form-text text-muted mb-3"><h6 style={{ color: "red" }}>กรุณาอัพโหลดหลักฐานการชำระเงิน</h6></small>
+              }
+            </div>
+
+            <section>
+              <div style={{ float: "left" }} className="mr-5">
+                <label style={{ display: "block" }} className="h5">วันเริ่มต้น</label>
+                <DatePicker
+                  style={{ display: "block" }}
+                  dateFormat='yyyy/MM/dd'
+                  selected={selectedStartDate}
+                  onChange={date => this.setState({ selectedStartDate: date })}
+                  isClearable
+                  showYearDropdown
+                  showMonthDropdown
+                  scrollableMonthYearDropdown
+                />
+              </div>
+              <div style={{ float: "left" }}>
+                <label style={{ display: "block" }} className="h5">วันสิ้นสุด</label>
+                <DatePicker
+                  style={{ display: "block" }}
+                  dateFormat='yyyy/MM/dd'
+                  selected={selectedExpireDate}
+                  onChange={date => this.setState({ selectedExpireDate: date })}
+                  isClearable
+                  showYearDropdown
+                  showMonthDropdown
+                  scrollableMonthYearDropdown
+                />
+              </div>
+            </section>
+          </div>
+        </div>
+
+
+        <button
+          type="button"
+          className="btn btn-danger ml-auto col-lg-1 col-md-6 "
+          onClick={() => this.setState({
+            emails: [],
+            selectedStartDate: null,
+            selectedExpireDate: null,
+            selectedFile: null
+          })}
+        >
+          ยกเลิก
+        </button>
+        <button
+          type="button"
+          className="btn btn-success col-lg-1 col-md-6 "
+          onClick={() => this.onUpload()}
+        >
+          ยืนยัน
+        </button>
+
+
       </div>
     )
   }
